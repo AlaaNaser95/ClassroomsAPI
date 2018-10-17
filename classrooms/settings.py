ca0ef26b52e09968ddf3f323ef5e7a9239f296a5
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-10-17 11:28:29 +0300

    task submission

--- classrooms/settings.py
+++ classrooms/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'classes',
+    'api',
 
     'crispy_forms',
+    'rest_framework',
+
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- classrooms/urls.py
+++ classrooms/urls.py
@@ -4,15 +4,30 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from classes import views
+from api.views import (
+    ClassroomListView,
+    ClassroomDetailView,
+    ClassroomUpdateView,
+    ClassroomDeleteView,
+    ClassroomCreateView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('classrooms/api/', ClassroomListView.as_view(), name='api-list'),
+    path('classrooms/api/<int:classroom_id>/', ClassroomDetailView.as_view(), name='api-detail'),
+
     path('classrooms/', views.classroom_list, name='classroom-list'),
     path('classrooms/<int:classroom_id>/', views.classroom_detail, name='classroom-detail'),
+
 
     path('classrooms/create', views.classroom_create, name='classroom-create'),
     path('classrooms/<int:classroom_id>/update/', views.classroom_update, name='classroom-update'),
     path('classrooms/<int:classroom_id>/delete/', views.classroom_delete, name='classroom-delete'),
+
+    path('classrooms/api/create', ClassroomCreateView.as_view(), name='api-create'),
+    path('classrooms/api/<int:classroom_id>/update/', ClassroomUpdateView.as_view(), name='api-update'),
+    path('classrooms/api/<int:classroom_id>/delete/', ClassroomDeleteView.as_view(), name='classroom-delete'),
 ]
 
 if settings.DEBUG:
